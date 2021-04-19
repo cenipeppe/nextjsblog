2e2a9dd94f3ec0a08444b7812c61281d1cb0cf12
Giuseppe Ceniviva <cenipeppe@outlook.it>
2021-04-19 14:42:25 +0200

    added aboutMe section in index

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import Post from "../components/Post";
 import { getAllPosts } from "../lib/data";
 import HeadCover from "../components/HeadCover";
 import { IoIosArrowDown } from "@react-icons/all-files/io/IoIosArrowDown";
+import { FaFacebook } from "@react-icons/all-files/fa/FaFacebook";
+import { AiFillInstagram } from "@react-icons/all-files/ai/AiFillInstagram";
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { useRef } from "react";
 
 export default function Home({ posts }) {
@@ -42,6 +45,32 @@ export default function Home({ posts }) {
         className="mx-auto my-7 w-9/12 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6"
         ref={myRef}
       >
+        <div className="lg:col-span-3 md:col-span-2 sm:col-span-1 mb-5 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
+          <div
+            className="col-span-1 w-full"
+            style={{
+              background: `url("https://images.unsplash.com/photo-1616149562385-1d84e79478bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80") no-repeat center center`,
+              backgroundSize: "cover",
+              height: "450px",
+              borderRadius: "1rem",
+            }}
+          />
+          <div className="col-span-1 flex flex-col justify-center px-10">
+            <h3 className="my-5">About me</h3>
+            <p className="mb-5">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
+              doloremque, est consequatur officia ipsum vitae similique
+              reprehenderit. Deserunt odio inventore mollitia ipsam ipsa odit
+              quidem non suscipit, laborum enim accusamus.
+            </p>
+            <div className="w-full flex justify-around items-center">
+              <FaFacebook size="3rem"/>
+              <AiFillInstagram size="3rem"/>
+              <FaLinkedin size="3rem"/>
+            </div>
+          </div>
+        </div>
+        <hr className="lg:col-span-3 md:col-span-2 sm:col-span-1" />
         <h2 className="lg:col-span-3 md:col-span-2 sm:col-span-1 mb-5">
           The lastest articles of blog
         </h2>
